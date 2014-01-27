6867c8b7b4e999fc9275f4ffefecbc4b1fc326b1
nsmarcus <ns.marcus@gmail.com>
2014-01-27 08:08:04 -0500

    webservice

---
+++ MovieDB/MovieDBApi/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using MovieDB.MovieDBShared;
+using MovieDBRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieDBApi.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        MovieRepository mr = new MovieRepository();
+        [HttpGet]
+        public IQueryable<Movie> Get()
+        {
+            return mr.Retrieve();
+        }
+    }
+}
---
+++ MovieDB/MovieDBApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieDBApi.WebApiApplication" Language="C#" %>
